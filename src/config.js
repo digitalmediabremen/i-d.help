9d6c9bdcc580d58ca113be7c465ca961f49d0788
Leo <130.0x82@gmail.com>
2021-09-09 11:13:34 +0200

    change api base to new id-help repository

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-// hej.digital configuration
+// i-d.help configuration
 
 // static labels are filtered globally and selected during inital page load
 export const STATIC_LABELS = ["First semester aid", "Corona Info"];
@@ -6,9 +6,9 @@ export const STATIC_LABELS = ["First semester aid", "Corona Info"];
 // manual exclusion of labels.
 // if you want to exclude labels automaticly,
 // let them start with a underscore '_'
-export const EXCLUDED_LABELS = ["orientation week"];
+export const EXCLUDED_LABELS = [];
 
-export const API_BASE_URL = "https://api.github.com/repos/digitalmediabremen/hej/";
+export const API_BASE_URL = "https://api.github.com/repos/digitalmediabremen/i-d.help/";
 // the oauth access token of a github user.
 // so posts from the frontend appear to be made by this user
 export const OAUTH_TOKEN = "00f69af9fb63058d73" + "" + "d9eb7cc6ba43e9d6410bd8";
