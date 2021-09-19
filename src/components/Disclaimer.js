e0fb98c29d8f104d4b9bb8b2eabfb80f42d5eba9
Leo <130.0x82@gmail.com>
2021-09-19 13:31:45 +0200

    fix classname error

--- src/components/Disclaimer.js
+++ src/components/Disclaimer.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Disclaimer = (props) => {
     return (
-        <div class="disclaimer">
-            <span class="small">
+        <div className="disclaimer">
+            <span className="small">
                 Made by Leonard Puhl, Jelko Arnds and Luca Iuzzolino ― Moderated by students of <a target="_blank" href="http://digitalmedia-bremen.de">digital media</a> at hfk. 
             </span>
         </div>
