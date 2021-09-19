0de6f5d84edfd1ec98b1ea77009bb230eedbf224
Leo <130.0x82@gmail.com>
2021-09-19 13:24:23 +0200

    add js css overwrites

--- src/config.js
+++ src/config.js
@@ -12,3 +12,14 @@ export const API_BASE_URL = "https://api.github.com/repos/digitalmediabremen/i-d
 // the oauth access token of a github user.
 // so posts from the frontend appear to be made by this user
 export const OAUTH_TOKEN = "ghp_vUZk36uyHLDHBD" + "" + "SqVdBIpG8eqCT3QZ0dyTnL";
+
+export const getCSSVariableOverwrites = () => {
+    return {
+        "--headline": "0,  255, 255",
+        "--text": "255,  255, 255",
+        "--textFilter": "14, 140, 133",
+        "--filter": "255,  255, 255",
+        "--background": "0, 140, 0",
+        "--font": "haptik, monospace",
+    }   
+}
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,42 +1,51 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect } from "react";
 
-import HomeView from 'containers/HomeView.js'
-import InputView from 'containers/InputView.js'
-import PreSelectionView from 'containers/PreSelectionView.js'
-import QuestionView from 'containers/QuestionView.js'
-import { Switch, Route, BrowserRouter, Redirect } from "react-router-dom"
-import "./App.css"
+import HomeView from "containers/HomeView.js";
+import InputView from "containers/InputView.js";
+import PreSelectionView from "containers/PreSelectionView.js";
+import QuestionView from "containers/QuestionView.js";
+import { Switch, Route, BrowserRouter, Redirect } from "react-router-dom";
+import "./App.css";
 import withSelectedFilters from "utils/withSelectedFilters.js";
+import { getCSSVariableOverwrites } from "../config";
 
-
-
-class App extends Component {
-
-  render() {
+const App = (props) => {
+    useLayoutEffect(() => {
+        const overwrites = getCSSVariableOverwrites();
+        if (!overwrites) return;
+        const cssOverwritesString = Object.entries(getCSSVariableOverwrites())
+            ?.map(([key, value]) => `${key}: ${value}`)
+            .join("; ");
+        document.body.style = cssOverwritesString;
+    }, []);
     return (
-      <BrowserRouter onUpdate={() => { window.scrollTo(0, 0); console.log("sdsd"); }}>
-        <div className="App">
-          <Switch>
-            {this.props.getSelectedStaticFilter() == undefined &&
-              <Route path="/" component={PreSelectionView} />
-            }
-            <Route exact path='/' component={HomeView} />
-            <Route path='/ask' component={InputView} />
-            {/* <Route path='/thanks' render={(props) => <HomeView {...props} thanks={true} />} /> */}
-            <Route path='/thanks'>
-              <Redirect
-                to={{
-                  pathname: "/",
-                  state: { thanks: true }
-                }}
-              />
-            </Route>
-            <Route path='/:number' component={QuestionView} />
-          </Switch>
-        </div>
-      </BrowserRouter>
-    )
-  }
-}
+        <BrowserRouter
+            onUpdate={() => {
+                window.scrollTo(0, 0);
+                console.log("sdsd");
+            }}
+        >
+            <div className="App">
+                <Switch>
+                    {props.getSelectedStaticFilter() == undefined && (
+                        <Route path="/" component={PreSelectionView} />
+                    )}
+                    <Route exact path="/" component={HomeView} />
+                    <Route path="/ask" component={InputView} />
+                    {/* <Route path='/thanks' render={(props) => <HomeView {...props} thanks={true} />} /> */}
+                    <Route path="/thanks">
+                        <Redirect
+                            to={{
+                                pathname: "/",
+                                state: { thanks: true },
+                            }}
+                        />
+                    </Route>
+                    <Route path="/:number" component={QuestionView} />
+                </Switch>
+            </div>
+        </BrowserRouter>
+    );
+};
 
 export default withSelectedFilters(App);
